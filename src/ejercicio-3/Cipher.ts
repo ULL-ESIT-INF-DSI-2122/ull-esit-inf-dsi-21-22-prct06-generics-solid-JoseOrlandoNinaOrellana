3220e4b24e22d58f330b0b4b2f492fbc9c03c665
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-03-23 21:34:29 +0000

    añadir caso en el que el caracter no pertenezca al alfabeto

--- src/ejercicio-3/Cipher.ts
+++ src/ejercicio-3/Cipher.ts
@@ -7,8 +7,12 @@ export class Cipher {
 
         for(let i: number = 0; i < originalText.length; ++i)
         {
-            displacement = this.alphabet.indexOf(this.key[i % this.key.length]) + 1;
-            cipherText += this.alphabet[(this.alphabet.indexOf(originalText[i]) + displacement) % this.alphabet.length];
+            if(this.alphabet.indexOf(originalText[i]) > -1) {
+                displacement = this.alphabet.indexOf(this.key[i % this.key.length]) + 1;
+                cipherText += this.alphabet[(this.alphabet.indexOf(originalText[i]) + displacement) % this.alphabet.length];
+            }
+            else
+                cipherText += originalText[i];
         }
 
         return cipherText;
@@ -20,11 +24,15 @@ export class Cipher {
 
         for(let i: number = 0; i < cipherText.length; ++i)
         {
-            displacement = this.alphabet.indexOf(this.key[i % this.key.length]) + 1;
-            if(this.alphabet.indexOf(cipherText[i]) >= displacement)
-                originalText += this.alphabet[(this.alphabet.indexOf(cipherText[i]) - displacement) % this.alphabet.length];
+            if(this.alphabet.indexOf(cipherText[i]) > -1) {
+                displacement = this.alphabet.indexOf(this.key[i % this.key.length]) + 1;
+                if(this.alphabet.indexOf(cipherText[i]) >= displacement)
+                    originalText += this.alphabet[(this.alphabet.indexOf(cipherText[i]) - displacement) % this.alphabet.length];
+                else
+                    originalText += this.alphabet[this.alphabet.length - (displacement - this.alphabet.indexOf(cipherText[i]))];
+            }
             else
-                originalText += this.alphabet[this.alphabet.length - (displacement - this.alphabet.indexOf(cipherText[i]))];
+                originalText += cipherText[i];
         }
 
         return originalText;
@@ -33,5 +41,5 @@ export class Cipher {
 
 let c: Cipher = new Cipher(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], "CLAVE");
 
-console.log(c.encrypt("HOLAESTOESUNAPRUEBA"));
-console.log(c.decrypt("KAMWJVFPAXXYBMWXPCW"));
+console.log(c.encrypt("HOLAESTOESUNAPRUEBA,"));
+console.log(c.decrypt("KAMWJVFPAXXYBMWXPCW,"));
