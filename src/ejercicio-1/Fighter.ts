8124d6702e92c9c5df7b1b0e389520be48891dcf
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-03-27 18:40:35 +0000

    clase fighter creada

--- src/ejercicio-1/Fighter.ts
+++ src/ejercicio-1/Fighter.ts
@@ -1,5 +1,5 @@
 export abstract class Fighter {
-    constructor(protected name: string, protected height: number, protected weight: number) {}
+    constructor(protected name: string, protected height: number, protected weight: number, protected HP: number, protected attack: number, protected defense: number, protected catchingPhrases: string[]) {}
 
     getName(): string {
         return this.name;
@@ -11,5 +11,29 @@ export abstract class Fighter {
 
     getWeight(): number {
         return this.weight;
+    }
+
+    getCatchinPhrases(): string[] {
+        return this.catchingPhrases;
+    }
+
+    getRandomPhrase(): string {
+        return this.catchingPhrases[Math.floor(Math.random() * this.catchingPhrases.length)];
+    }
+
+    getHP(): number {
+        return this.HP;
+    }
+
+    getAttack(): number {
+        return this.attack;
+    }
+
+    getDefense(): number {
+        return this.defense;
+    }
+
+    setHP(newHP: number) {
+        this.HP = newHP;
     }
 }
