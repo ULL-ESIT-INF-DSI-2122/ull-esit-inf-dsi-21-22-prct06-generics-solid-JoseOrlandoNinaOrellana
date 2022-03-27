71456db528612ffd98ca196a6f19c299231cd717
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-03-27 11:33:13 +0000

    añadir mas tests

--- tests/ejercicio-3.spec.ts
+++ tests/ejercicio-3.spec.ts
@@ -3,7 +3,16 @@ import { expect } from "chai";
 import { Cipher } from "../src/ejercicio-3/Cipher";
 
 describe("Cipher class tests", () => {
-    let cipher: Cipher = new Cipher(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], "CLAVE");
+    let alphabet: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cipher: Cipher = new Cipher(alphabet, "CLAVE");
+
+    it("getAlphabet function test", () => {
+        expect(cipher.getAlphabet()).to.be.equal(alphabet);
+    });
+
+    it("getKey function test", () => {
+        expect(cipher.getKey()).to.be.equal("CLAVE");
+    });
 
     it("encrypt function test", () => {
         expect(cipher.encrypt("HOLAESTOESUNAPRUEBA")).to.be.equal("KAMWJVFPAXXYBMWXPCW");
